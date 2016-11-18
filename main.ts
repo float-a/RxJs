f6b48cda99b05d6d4a0db73fdc132560759849a6
Adrian <adrian.ardelean95@gmail.com>
2016-11-18 16:55:37 +0200

    Add circle element and get movies.

--- main.ts
+++ main.ts
@@ -99,3 +99,54 @@ source4.subscribe(value => console.log(`value: ${value}`),
                     () =>  console.log("completed")
 )
 
+
+//Ex. 5
+let circle = document.getElementById("circle");
+
+let source5 = Observable.fromEvent(document, "mousemove")
+                        .map((e : MouseEvent) => {
+                            return {
+                                x: e.clientX,
+                                y: e.clientY
+                            }
+                        })
+                        .filter(value => value.x < 500)
+                        .delay(300);
+                        
+
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
+
+source5.subscribe(onNext,
+                    e =>   console.log(`error: ${e}`),
+                    () =>  console.log("completed")
+)
+
+//Ex. 6
+let output = document.getElementById("output");
+let button = document.getElementById("button")
+let source6 = Observable.fromEvent(button, "click");
+ 
+
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerHTML = m.title;
+            output.appendChild(div);
+        })
+    })
+
+    xhr.open("GET",url);
+    xhr.send();
+}
+
+source6.subscribe(e => load("movies.json"),
+                    e =>   console.log(`error: ${e}`),
+                    () =>  console.log("completed")
+)
